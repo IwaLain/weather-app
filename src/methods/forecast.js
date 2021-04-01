bfc483b32efd4af0dc39ae41f546ec48e021ef2f
IwaLain <arthes.menetil@gmail.com>
2021-04-01 13:39:09 +0300

    using metric system

--- src/methods/forecast.js
+++ src/methods/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,daily,alerts&appid=4200e02031ad31ba70b9a5f2492c7d31`
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,daily,alerts&units=metric&appid=4200e02031ad31ba70b9a5f2492c7d31`
     
     request({url, json: true}, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.cod) {
             callback('Wrong coordinates. Try another.', undefined)
         } else {
-            callback(undefined, `${body.hourly[0].weather[0].main}. Forecast for next hour: ${(body.hourly[0].temp-273).toPrecision(3)} degrees. Feels like ${(body.hourly[0].feels_like-273).toPrecision(3)}.`)
+            callback(undefined, `${body.hourly[0].weather[0].main}. Forecast for next hour: ${(body.hourly[0].temp).toPrecision(3)} degrees. Feels like ${(body.hourly[0].feels_like).toPrecision(3)}.`)
         }
     })
 }
